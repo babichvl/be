4d8f72d10856ee6182f9fb224332400612ea79a7
babichvl <babich13vl@gmail.com>
2026-09-01 18:54:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-
+const SUPABASE_URL = 'https://qhvtapqlyajkikgfacdo.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFodnRhcHFseWFqa2lrZ2ZhY2RvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNjM3NjEsImV4cCI6MjEwMzczOTc2MX0.hr8Uiy3hvbhwfJ0At7T0TR8waK4Mt5ylFw-B-qp5Cow';
